9efe22ddb74d4d8d6462baa53982f1d4861c073c
darkoo59 <darko.selakovic11@gmail.com>
2022-06-17 13:37:23 +0200

    Vue.js and spark integration

--- FitPass/src/main/java/sample/hello.java
+++ FitPass/src/main/java/sample/hello.java
@@ -1,22 +1,28 @@
 package sample;
 import com.google.gson.Gson;
+import spark.Filter;
 
 import static spark.Spark.*;
 
 import java.io.File;
+import java.net.URL;
 
 
 public class hello {
     private static Gson gson = new Gson();
 
     public static void main(String[] args) throws Exception {
-        staticFileLocation("static");
-        //staticFiles.externalLocation(new File("./static").getCanonicalPath());
+        staticFileLocation("static/vue/dist");
         port(8080);
         // Site pages
-        get("/index" , (req,res) -> {
+        after((Filter) (req, res) -> {
+            res.header("Acces-Control-Allow-Origin","*");
+            res.header("Acces-Control-Allow-Methods", "GET");
+        });
+        get("/something" , (req,res) -> {
+            res.type("application/html");
             res.redirect("index.html");
-            return null;
+            return "";
         });
     }
 }
